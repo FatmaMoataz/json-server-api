a92f29072f9d64eb3735f0cc7acd0106be0090f1
FatmaMoataz <fatmamoataz65@gmail.com>
2025-05-19 17:50:31 +0200

    fix: correct start script and server config

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => { 
   console.log(`JSON Server is running on port ${PORT}`);
 });
